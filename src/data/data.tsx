c04e518d76ee7222c923e2b36a851a41859a46ac
Haseen Afridi <haseenafridi10@gmail.com>
2025-04-06 13:02:26 +0500

    second commit

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -23,7 +23,7 @@ import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
 import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
 import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
 import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
-import profilepic from '/public/images/banner_bg.jpeg';
+import profilepic from '../images/profilePic.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
   About,
