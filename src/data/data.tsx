6b7e50b772b5ad88aa27d64478c3023c5e1e7b8b
Haseen Afridi <haseenafridi10@gmail.com>
2025-04-06 13:36:13 +0500

    third commit

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -8,8 +8,8 @@ import {
   SparklesIcon,
 } from '@heroicons/react/24/outline';
 
-import GithubIcon from '../components/Icon/GithubIcon';
 import FacebookIcon from "../components/Icon/FacebookIcon";
+import GithubIcon from '../components/Icon/GithubIcon';
 import InstagramIcon from '../components/Icon/InstagramIcon';
 import heroImage from '../images/header-background.webp';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
@@ -50,9 +50,9 @@ export const homePageMeta: HomepageMeta = {
  * Section definition
  */
 export const SectionId = {
-  Hero: 'hero',
   About: 'about',
   Contact: 'contact',
+  Hero: 'hero',
   Portfolio: 'portfolio',
   Resume: 'resume',
   Skills: 'skills',
@@ -66,8 +66,19 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  * Hero section
  */
 export const heroData: Hero = {
-  imageSrc: heroImage,
-  name: `I'm Haseen Afridi.`,
+  actions: [
+    {
+      href: '/assets/resume.pdf',
+      Icon: ArrowDownTrayIcon,
+      primary: true,
+      text: 'Resume',
+    },
+    {
+      href: `#${SectionId.Contact}`,
+      primary: false,
+      text: 'Contact',
+    },
+  ],
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
@@ -76,42 +87,31 @@ export const heroData: Hero = {
         registrar and site builder.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-      In my free time, I love exploring new adventures whether it's <strong className="text-stone-100">hiking</strong>,
-      hrough scenic trails, capturing moments with <strong className="text-stone-100">photography</strong>
-      , or diving into creative <strong className="text-stone-100">coding projects</strong> that bring ideas to life.{' '}
+        In my free time, I love exploring new adventures whether it's <strong className="text-stone-100">hiking</strong>,
+        through scenic trails, capturing moments with <strong className="text-stone-100">photography</strong>
+        , or diving into creative <strong className="text-stone-100">coding projects</strong> that bring ideas to life.{' '}
       </p>
     </>
   ),
-  actions: [
-    {
-      href: '/assets/resume.pdf',
-      text: 'Resume',
-      primary: true,
-      Icon: ArrowDownTrayIcon,
-    },
-    {
-      href: `#${SectionId.Contact}`,
-      text: 'Contact',
-      primary: false,
-    },
-  ],
+  imageSrc: heroImage,
+  name: `I'm Haseen Afridi.`,
 };
 
 /**
  * About section
  */
 export const aboutData: About = {
-  profileImageSrc: profilepic,
+  aboutItems: [
+    { Icon: MapIcon, label: 'Location', text: 'Karachi' },
+    { Icon: CalendarIcon, label: 'Age', text: '20' },
+    { Icon: FlagIcon, label: 'Nationality', text: 'Pakistani' },
+    { Icon: SparklesIcon, label: 'Interests', text: 'Sports, Gym' },
+    { Icon: AcademicCapIcon, label: 'Study', text: 'Ned University' },
+    { Icon: BuildingOffice2Icon, label: 'Employment', text: 'Seeking internship' },
+  ],
   description: `As a Front-end Developer, my key purpose is to build scalable, responsive
   and secure websites using latest Tech stacks.`,
-  aboutItems: [
-    {label: 'Location', text: 'Karachi', Icon: MapIcon},
-    {label: 'Age', text: '20', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Pakistani', Icon: FlagIcon},
-    {label: 'Interests', text: 'Sports, Gym', Icon: SparklesIcon},
-    {label: 'Study', text: 'Ned University', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Seeking internship', Icon: BuildingOffice2Icon},
-  ],
+  profileImageSrc: profilepic,
 };
 
 /**
@@ -119,188 +119,59 @@ export const aboutData: About = {
  */
 export const skills: SkillGroup[] = [
   {
-    name: 'Spoken languages',
-    skills: [
-      {
-        name: 'English',
-        level: 10,
-      },
-      {
-        name: 'Urdu',
-        level: 4,
-      },
-      {
-        name: 'Turkish',
-        level: 1,
-      },
-    ],
-  },
-  {
     name: 'Frontend development',
     skills: [
-      {
-        name: 'React',
-        level: 9,
-      },
-      {
-        name: 'Typescript',
-        level: 7,
-      },
-      {
-        name: 'Nextjs',
-        level: 6,
-      },
+      { level: 9, name: 'React' },
+      { level: 7, name: 'Typescript' },
+      { level: 6, name: 'Nextjs' },
     ],
   },
   {
     name: 'Backend development',
     skills: [
-      {
-        name: 'Node.js',
-        level: 8,
-      },
-      {
-        name: 'Rust',
-        level: 5,
-      },
-      {
-        name: 'Golang',
-        level: 4,
-      },
+      { level: 8, name: 'Node.js' },
+      { level: 5, name: 'Rust' },
+      { level: 4, name: 'Golang' },
     ],
   },
-  // {
-  //   name: 'Mobile development',
-  //   skills: [
-  //     {
-  //       name: 'React Native',
-  //       level: 9,
-  //     },
-  //     {
-  //       name: 'Flutter',
-  //       level: 4,
-  //     },
-  //     {
-  //       name: 'Swift',
-  //       level: 3,
-  //     },
-  //   ],
-  // },
+  {
+    name: 'Spoken languages',
+    skills: [
+      { level: 10, name: 'English' },
+      { level: 4, name: 'Urdu' },
+      { level: 1, name: 'Turkish' },
+    ],
+  },
 ];
 
 /**
  * Portfolio section
  */
 export const portfolioItems: PortfolioItem[] = [
-  {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage1,
-  },
-  {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage2,
-  },
-  {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage3,
-  },
-  {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage4,
-  },
-  {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage5,
-  },
-  {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage7,
-  },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage11,
-  },
+  { description: 'Give a short description of your project here.', image: porfolioImage1, title: 'Project title 1', url: 'https://reactresume.com' },
+  { description: 'Give a short description of your project here.', image: porfolioImage2, title: 'Project title 2', url: 'https://reactresume.com' },
+  { description: 'Give a short description of your project here.', image: porfolioImage3, title: 'Project title 3', url: 'https://reactresume.com' },
+  { description: 'Give a short description of your project here.', image: porfolioImage4, title: 'Project title 4', url: 'https://reactresume.com' },
+  { description: 'Give a short description of your project here.', image: porfolioImage5, title: 'Project title 5', url: 'https://reactresume.com' },
+  { description: 'Give a short description of your project here.', image: porfolioImage6, title: 'Project title 6', url: 'https://reactresume.com' },
+  { description: 'Give a short description of your project here.', image: porfolioImage7, title: 'Project title 7', url: 'https://reactresume.com' },
+  { description: 'Give a short description of your project here.', image: porfolioImage8, title: 'Project title 8', url: 'https://reactresume.com' },
+  { description: 'Give a short description of your project here.', image: porfolioImage9, title: 'Project title 9', url: 'https://reactresume.com' },
+  { description: 'Give a short description of your project here.', image: porfolioImage10, title: 'Project title 10', url: 'https://reactresume.com' },
+  { description: 'Give a short description of your project here.', image: porfolioImage11, title: 'Project title 11', url: 'https://reactresume.com' },
 ];
 
 /**
  * Resume section -- TODO: Standardize resume contact format or offer MDX
  */
 export const education: TimelineItem[] = [
-  {
-    date: 'October 2023',
-    location: 'Ned University',
-    title: 'Bachelors in Telecommunication',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
-  {
-    date: 'March 2021',
-    location: 'FalconHouse Grammar School',
-    title: 'Matriculation',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
+  { date: 'October 2023', location: 'Ned University', title: 'Bachelors in Telecommunication', content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p> },
+  { date: 'March 2021', location: 'FalconHouse Grammar School', title: 'Matriculation', content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p> },
 ];
 
 export const experience: TimelineItem[] = [
-  {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
-    content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
-    ),
-  },
-  {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
-    content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
-    ),
-  },
+  { date: 'March 2010 - Present', location: 'Awesome Development Company', title: 'Senior UX Engineer', content: <p>Describe work, special projects, notable achievements, what technologies you have been working with, and anything else that would be useful for an employer to know.</p> },
+  { date: 'March 2007 - February 2010', location: 'Garage Startup Studio', title: 'Junior bug fixer', content: <p>Describe work, special projects, notable achievements, what technologies you have been working with, and anything else that would be useful for an employer to know.</p> },
 ];
 
 /**
@@ -309,51 +180,23 @@ export const experience: TimelineItem[] = [
 export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
-    {
-      name: 'Haseen Khan',
-      text: 'I’ve been thoroughly impressed by this portfolio! The design is sleek, modern, and incredibly user-friendly. Every project is showcased beautifully, and the attention to detail is evident. As a web designer myself, I really appreciate the clean aesthetic and seamless navigation. It’s clear that a lot of thought and effort went into every aspect of this portfolio. Highly recommend checking it out!',
-      image: 'https://th.bing.com/th/id/OIP.IhHfxrnrxZcij7AAwZrnZQAAAA?w=115&h=190&c=7&r=0&o=5&dpr=1.3&pid=1.7',
-    },
-    {
-      name: 'Omer Khan',
-      text: 'This portfolio is a great example of how to effectively present both technical skills and creativity. I was particularly impressed by how each project is explained with clarity, showing the thought process behind every solution. It’s not only visually appealing, but it also demonstrates a high level of professionalism and skill in the work. This portfolio is a fantastic showcase of expertise and dedication!',
-      image: 'https://th.bing.com/th/id/OIP.IzipPT7JyLJxY7HbTWRUkAHaHa?w=190&h=190&c=7&r=0&o=5&dpr=1.3&pid=1.7',
-    },
-    {
-      name: 'Someone Me',
-      text: `I absolutely love how this portfolio captures the essence of both creativity and practicality. It’s clear that the creator has a deep understanding of design principles, but what really stands out is the ability to translate those skills into real-world applications. The projects are diverse, and the layout is clean and easy to navigate. It's the perfect example of a professional portfolio that speaks volumes about someone's capabilities.`,
-      image: 'https://th.bing.com/th/id/OIP.47sG9d2l51fPG3gIr7CbEgHaHa?w=207&h=207&c=7&r=0&o=5&dpr=1.3&pid=1.7',
-    },
+    { name: 'Haseen Khan', text: 'I’ve been thoroughly impressed by this portfolio!...', image: 'https://th.bing.com/th/id/OIP.IhHfxrnrxZcij7AAwZrnZQAAAA?w=115&h=190&c=7&r=0&o=5&dpr=1.3&pid=1.7' },
+    { name: 'Omer Khan', text: 'This portfolio is a great example of how to effectively present...', image: 'https://th.bing.com/th/id/OIP.IzipPT7JyLJxY7HbTWRUkAHaHa?w=190&h=190&c=7&r=0&o=5&dpr=1.3&pid=1.7' },
+    { name: 'Someone Me', text: 'I absolutely love how this portfolio captures...', image: 'https://th.bing.com/th/id/OIP.47sG9d2l51fPG3gIr7CbEgHaHa?w=207&h=207&c=7&r=0&o=5&dpr=1.3&pid=1.7' },
   ],
 };
 
 /**
  * Contact section
  */
-
 export const contact: ContactSection = {
-  headerText: 'Get in touch.',
   description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  headerText: 'Get in touch.',
   items: [
-    {
-      type: ContactType.Email,
-      text: 'haseenafridi10@gmail.com',
-    },
-    {
-      type: ContactType.Location,
-      text: 'Karachi, Pakistan',
-      // href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
-    },
-    {
-      type: ContactType.Instagram,
-      text: '@haseen__afridi',
-      href: 'https://www.instagram.com/tbakerx/',
-    },
-    {
-      type: ContactType.Github,
-      text: 'haseenafridi10',
-      href: 'https://github.com/tbakerx',
-    },
+    { type: ContactType.Email, text: 'haseenafridi10@gmail.com' },
+    { type: ContactType.Location, text: 'Karachi, Pakistan' },
+    { type: ContactType.Instagram, text: '@haseen__afridi', href: 'https://www.instagram.com/tbakerx/' },
+    { type: ContactType.Github, text: 'haseenafridi10', href: 'https://github.com/tbakerx' },
   ],
 };
 
@@ -361,9 +204,7 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/haseenafridi10'},
-  // {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'Facebook', Icon: FacebookIcon, href: 'https://www.facebook.com/haseenafridi10@gmail.com'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/haseen__afridi/'},
-  // {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  { label: 'Facebook', Icon: FacebookIcon, href: 'https://www.facebook.com/haseenafridi10@gmail.com' },
+  { label: 'Github', Icon: GithubIcon, href: 'https://github.com/haseenafridi10' },
+  { label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/haseen__afridi/' },
 ];
